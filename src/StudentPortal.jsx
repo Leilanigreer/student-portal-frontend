b97d954a0cf1f75d55c6303d34b11c02be18b1ef
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-11-03 14:05:13 -0500

    index and edit update

--- src/StudentPortal.jsx
+++ src/StudentPortal.jsx
@@ -1,7 +1,9 @@
+import { StudentProfileIndex } from "./StudentProfileIndex"
 export function StudentPortal() {
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <StudentProfileIndex />
     </main>
   )
 }
