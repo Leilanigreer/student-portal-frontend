a442293fcd2f5b9cf5af2824416670d91066c1fb
Leilanigreer <leilani@lpcgolf.com>
2024-11-03 08:54:22 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { StudentPortal } from "./StudentPortal";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <StudentPortal />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/StudentPortal.jsx
@@ -0,0 +1,7 @@
+export function StudentPortal() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  )
+}
